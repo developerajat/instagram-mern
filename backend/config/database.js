684a128d511e65b65dfbdd4a83051762ff215600
Rajat Sharma <rajatks90@gmail.com>
2024-10-23 20:07:40 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect("mongodb+srv://rajatks90:v1Pc9GWy3mfjYrwV@instaclone.5c3cz.mongodb.net/?retryWrites=true&w=majority&appName=instaclone", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongoose Connected");
     }).catch((error) => {
